25be0a88fce94bb5a17c18def2b2f136522b6e52
Marcel Ferreira <48413106+marceelrf@users.noreply.github.com>
2019-09-08 12:52:10 -0300

    Update DavidGraph.R

--- DavidGraph.R
+++ DavidGraph.R
@@ -5,5 +5,8 @@ df %>% #Dataframe saída do DavidTools
   ggplot(aes(x = Term, y = Count, fill = FDR)) +
   geom_bar(stat = "identity") +
   coord_flip() +
+  xlab("Termos") + #Labels
+  ylab("Percentagem de genes (%)") + #Labels
+  scale_fill_gradient(low="red", high="purple") + #Escolher cores
   theme_bw()
   
