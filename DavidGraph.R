3670c3fc4949b14a2996912901d0f8cb52ba0bec
Marcel Ferreira <48413106+marceelrf@users.noreply.github.com>
2019-09-08 12:26:20 -0300

    Update DavidGraph.R

--- DavidGraph.R
+++ DavidGraph.R
@@ -1,2 +1,9 @@
 library(ggplot2)
 library(dplyr)
+
+df %>% #Dataframe saída do DavidTools
+  ggplot(aes(x = Term, y = Count, fill = FDR)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  theme_bw()
+  
